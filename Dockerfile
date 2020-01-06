64bd3864e5acd5187a947f3149ed2c76e2759b04
Blek <Artur.Blek@de.rhenus.com>
2020-01-06 20:32:04 +0100

    initial

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM mcr.microsoft.com/dotnet/core/aspnet:2.2-stretch-slim AS base
+WORKDIR /app
+EXPOSE 80
+EXPOSE 443
+RUN apt-get update
+RUN apt-get install -y libgif7 libjpeg62 libopenjp2-7 libpng16-16 libtiff5 libwebp6
+RUN apt-get install -y gcc
+RUN ln -s /app/x64/liblept1753.so /usr/lib/x86_64-linux-gnu/liblept.so.5
+
+FROM mcr.microsoft.com/dotnet/core/sdk:2.2-stretch AS build
+WORKDIR /src
+COPY ["WebOcr/WebOcr.csproj", "WebOcr/"]
+RUN dotnet restore "WebOcr/WebOcr.csproj"
+COPY . .
+WORKDIR "/src/WebOcr"
+RUN dotnet build "WebOcr.csproj" -c Release -o /app
+
+FROM build AS publish
+RUN dotnet publish "WebOcr.csproj" -c Release -o /app
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app .
+ENTRYPOINT ["dotnet", "WebOcr.dll"]
---
+++ WebOcr/Controllers/OcrController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Tesseract;
+
+namespace WebOcr.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class OcrController : ControllerBase
+    {
+        // GET api/values
+        [HttpPost]
+        public async Task<IActionResult> Base64Tiff()
+        {
+            String base64;
+            using (StreamReader reader = new StreamReader(Request.Body, Encoding.UTF8))
+            {
+                base64 = await reader.ReadToEndAsync();
+            }
+            if (base64.Length == 0)
+                return BadRequest();
+            try
+            {
+                Console.WriteLine(base64);
+                byte[] bytes = Convert.FromBase64String(base64);
+                var engine = new TesseractEngine(@"./tessdata", "deu", EngineMode.TesseractAndCube);
+                var img = Pix.LoadTiffFromMemory(bytes);
+                var page = engine.Process(img, PageSegMode.AutoOsd);
+                String text = page.GetText();
+                return Content(text, "plain/text");
+            }
+            catch
+            {
+                return new StatusCodeResult(500);
+            }
+        }
+    }
+}
