412187df7a272d53e1980b410847135402d49942
Blek <Artur.Blek@de.rhenus.com>
2020-01-06 21:01:15 +0100

    fix gitignore

---
+++ tesseract/Interop/Constants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Tesseract.Interop
+{
+    /// <summary>
+    /// Description of Constants.
+    /// </summary>
+    internal static class Constants
+    {
+        public const string LeptonicaDllName = "liblept1780";
+        public const string TesseractDllName = "libtesseract4100";
+        
+        // tesseract uses an int to represent true false values.
+        public const int TRUE = 1;
+        public const int FALSE = 0;
+    }
+}
