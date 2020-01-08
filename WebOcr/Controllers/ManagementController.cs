227f9fa8a2226a52d6b4c673bd4b7ef1084e1f01
chixlol <artur.blek@web.de>
2020-01-08 21:34:07 +0100

    Get all configuration at once

--- WebOcr/Controllers/ManagementController.cs
+++ WebOcr/Controllers/ManagementController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebOcr.Models;
 using WebOcr.Services;
 
 namespace WebOcr.Controllers
@@ -14,6 +15,17 @@ namespace WebOcr.Controllers
         public ManagementController(ManagementService managementService)
         {
             this.managementService = managementService;
+        }
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+            WebTesseractStats stats = new WebTesseractStats()
+            {
+                Languages = managementService.GetAvailableLanguages(),
+                Qualities = Enum.GetNames(typeof(TessdataType)).ToList()
+            };
+            return Ok(stats);
         }
 
         public IActionResult Languages()
---
+++ WebOcr/Models/WebTesseractStats.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebOcr.Models
+{
+    public class WebTesseractStats
+    {
+        public List<String> Languages { get; set; }
+        public List<String> Qualities { get; set; }
+    }
+}
